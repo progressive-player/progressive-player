9d5c20031f50a88ea128283b6bbe897134794648
DamenH <damen@damenhannah.com>
2020-06-09 11:07:03 -0500

    Remove compression plugins.

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,8 @@
-const CompressionPlugin = require("compression-webpack-plugin");
-const BrotliPlugin = require("brotli-webpack-plugin");
-
 module.exports = {
     chainWebpack: config =>
     {
         if (process.env.NODE_ENV === 'production')
         {
-            config.plugin("CompressionPlugin").use(CompressionPlugin);
-            config.plugin("BrotliPlugin").use(BrotliPlugin);
             config.resolve.alias.set("vue$", "vue/dist/vue.runtime.esm-browser.prod.js");
         }
     }
