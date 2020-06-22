0da0b4d3f2969c86d7482802e94c5a602d6cac71
DamenH <damen@damenhannah.com>
2020-06-22 15:35:31 -0500

    Fixing possible issue.

--- transform.js
+++ transform.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 var bundle = fs.readFileSync('public/bundle.js', 'utf-8');
 var index = fs.readFileSync('public/index.html', 'utf-8');
 
-bundle = bundle.split('\\n        ').join('');
-bundle = bundle.split('\\n    ').join('');
-bundle = bundle.split('   ').join('');
+// bundle = bundle.split('\\n        ').join('');
+// bundle = bundle.split('\\n    ').join('');
+// bundle = bundle.split('   ').join('');
 
 index = index.split('\n').join('');
 index = index.split('\t').join('');
